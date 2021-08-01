42f9465931b9563c92939c7ea9679fc90d4f967a
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-01 11:14:30 -0400

    Move projects to separate component, tweak styling

--- src/Body.js
+++ src/Body.js
@@ -1,51 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button, Card, Icon, Image } from 'semantic-ui-react'
+import Projects from './Projects';
 
 function Body() {
-    const [projects, setProjects] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost:9393/projects')
-            .then(response => response.json())
-            .then(data => setProjects(data.projects))
-            .catch(error => console.log(error))
-    }, [])
-
-    const displayProjects = projects.map(project => {
-        return (
-            <Card key={project.id}>
-                <Image src={project.image} />
-                <Card.Content>
-                    <Card.Header>{project.name}</Card.Header>
-                    <Card.Description>{project.text}</Card.Description>
-                    <Card.Meta>
-                        <div className="tag-list">
-                            {project.tags.map(tag => {
-                                return (
-
-                                    <span className="tag">{tag.name}</span>
-
-                                )
-                            })}
-                        </div>
-                    </Card.Meta>
-                </Card.Content>
-                <Card.Content extra>
-                    <a>
-                        <Icon name='like' />
-                        {project.likes}
-                    </a>
-                    <a href={project.url}><Button className="blue-button">Live Website</Button></a>
-                </Card.Content>
-            </Card>
-        )
-    })
-
     return (
         <main>
-            <Card.Group centered fluid="true">
-                {displayProjects}
-            </Card.Group>
+            <Projects />
         </main>
     )
 }
---
+++ src/Projects.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { Button, Card, Icon, Image } from 'semantic-ui-react'
+
+function Projects() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9393/projects')
+            .then(response => response.json())
+            .then(data => setProjects(data.projects))
+            .catch(error => console.log(error))
+    }, [])
+
+    const displayProjects = projects.map(project => {
+        return (
+            <Card key={project.id}>
+                <Image src={project.image} />
+                <Card.Content>
+                    <Card.Header>{project.name}</Card.Header>
+                    <Card.Description>{project.text}</Card.Description>
+                    <Card.Meta>
+                        <div className="tag-list">
+                            {project.tags.map(tag => {
+                                return (
+
+                                    <span className="tag">{tag.name}</span>
+
+                                )
+                            })}
+                        </div>
+                    </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                    <span>
+                        <Icon name='like' />
+                        {project.likes}
+                    </span>
+                    <a href={project.url}><Button className="blue-button">Live Website</Button></a>
+                </Card.Content>
+            </Card>
+        )
+    })
+    
+    return (
+        <>
+            <h1 id="Projects">Projects</h1>
+            <Card.Group centered fluid="true">
+                {displayProjects}
+            </Card.Group>
+        </>
+    )
+}
+
+export default Projects;
