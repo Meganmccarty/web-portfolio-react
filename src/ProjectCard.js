c0b49668c1fcec85b769d0b8cdd57a60aff69831
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-01 16:09:07 -0400

    Create new component for each project

---
+++ src/ProjectCard.js
@@ -0,0 +1,50 @@
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+function ProjectCard({ id, name, image, text, url, likes, tags, onPatch }) {
+
+    function handleClick(e) {
+        let likesToPatch = parseInt(e.target.nextSibling.innerText, 10) + 1;
+        const configObj = {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                'likes': parseInt(likesToPatch)
+            })
+        }
+        fetch(`http://localhost:9393/projects/${id}`, configObj)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                onPatch(data)
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <Card>
+            <Image src={image} />
+            <Card.Content>
+                <Card.Header>{name}</Card.Header>
+                <Card.Description>{text}</Card.Description>
+                <Card.Meta>
+                    <div className="tag-list">
+                        {tags.map(tag => {
+                            return (
+                                <span key={tag.name} className="tag">{tag.name}</span>
+                            )
+                        })}
+                    </div>
+                </Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+                <Icon name='like' onClick={(e) => handleClick(e)} />
+                <span>{likes}</span>
+                <a href={url}><button className="ui button blue-button">Live Website</button></a>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default ProjectCard;
