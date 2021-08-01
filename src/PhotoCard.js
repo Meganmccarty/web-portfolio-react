9c2a90c4cb0776b73b5675b48402e5ea500edeb6
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-01 11:58:52 -0400

    Add img alt text

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -3,7 +3,7 @@ import Photo from './images/mccarty-photo.jpeg';
 function PhotoCard() {
     return (
         <section>
-            <img src={Photo}></img>
+            <img src={Photo} alt="Software Engineer Megan McCarty"></img>
             <h1>Megan McCarty</h1>
             <h2>Software Engineer</h2>
         </section>
