3933d0972caab486a32fcea0746466bff5c361e7
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-04 12:08:35 -0400

    Fix typo in fetch url

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -13,9 +13,12 @@ function ProjectCard({ id, name, image, text, url, likes, tags, onPatch }) {
                 'likes': parseInt(likesToPatch)
             })
         }
-        fetch(`http://localhost9393/projects/${id}`, configObj)
+        fetch(`http://localhost:9393/projects/${id}`, configObj)
             .then(response => response.json())
-            .then(data => onPatch(data))
+            .then(data => {
+                console.log(data)
+                onPatch(data)
+            })
             .catch(error => console.log(error))
     }
 
